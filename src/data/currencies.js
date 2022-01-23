c23a5fd7a222d65dac842e1d54b2a6d855e00d24
Uriel Aispuro <urielalexis64@gmail.com>
2022-01-23 00:24:22 -0700

    add currencies data

---
+++ src/data/currencies.js
@@ -0,0 +1,20 @@
+const currencies = [
+	{
+		id: "USD",
+		name: "United States dollar",
+	},
+	{
+		id: "MXN",
+		name: "Mexican peso",
+	},
+	{
+		id: "EUR",
+		name: "Euro",
+	},
+	{
+		id: "GBP",
+		name: "Pound sterling",
+	},
+];
+
+export {currencies};
